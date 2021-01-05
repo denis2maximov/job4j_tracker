9c92c4c76eeb4ed8b76ef1d45ad89a275bf8c3f3
games-pc <denis2maksimov@gmail.com>
2021-01-05 17:28:03 +0300

    6. Метод замены заявки. Tracker.replace [#394986]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,16 +7,20 @@ public class StartUI {
     public static void main(String[] args) {
     Tracker take = new Tracker();
     Item kata = new Item();
-        Item kata1 = new Item();
-        Item olia = new Item();
-         take.add(kata);
+    Item kata1 = new Item();
+    Item olia = new Item();
+    take.add(kata);
     kata.setName("Катя");
-        take.add(kata1);
-        kata1.setName("Катя");
-        take.add(olia);
-        olia.setName("Оля");
+    take.add(kata1);
+    kata1.setName("Катя");
+    take.add(olia);
+    olia.setName("Оля");
 System.out.println(take.findById(1));
  System.out.println(take.findByName("Оля"));
  System.out.println((take.findByName("Катя")));
+ System.out.println((take.findAll()));
+ System.out.println((take.replace(1, kata1)));
+
+
       }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,7 +14,7 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
+  /*  public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
@@ -24,7 +24,11 @@ public class Tracker {
             }
         }
         return rsl;
-    }
+    } */
+  public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+  }
 
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
@@ -53,6 +57,27 @@ public class Tracker {
             if (Objects.equals(item.getName(), key)) {
              rsl[x++] = items[index];
               rsl = Arrays.copyOf(items, rsl.length);
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+            }
+        items[index] = item;
+        item.setId(id);
+        return true;
+           }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
             }
         }
         return rsl;
