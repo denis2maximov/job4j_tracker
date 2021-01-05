b93e2ea393b4cfb53ce9beb92a061bfb10c9e074
games-pc <denis2maksimov@gmail.com>
2021-01-05 16:07:08 +0300

    5. Tracker - хранилище [#394991]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,7 +7,16 @@ public class StartUI {
     public static void main(String[] args) {
     Tracker take = new Tracker();
     Item kata = new Item();
-    take.add(kata);
-   System.out.println(take.findById(1));
-    }
+        Item kata1 = new Item();
+        Item olia = new Item();
+         take.add(kata);
+    kata.setName("Катя");
+        take.add(kata1);
+        kata1.setName("Катя");
+        take.add(olia);
+        olia.setName("Оля");
+System.out.println(take.findById(1));
+ System.out.println(take.findByName("Оля"));
+ System.out.println((take.findByName("Катя")));
+      }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Tracker {
     private final Item[] items = new Item[100];
@@ -8,10 +9,10 @@ public class Tracker {
     private int size = 0;
 
     public Item add(Item item) {
-         item.setId(ids++);
-         items[size++] = item;
-         return item;
-     }
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
 
     public Item findById(int id) {
         Item rsl = null;
@@ -25,22 +26,38 @@ public class Tracker {
         return rsl;
     }
 
- public Item[] findAll() {
-        Item[] rsl;
-        rsl = Arrays.copyOf(items, size);
-     return rsl;
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 
-    public Item findByName(String key) {
-        Item rsl = null;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tracker tracker = (Tracker) o;
+        return ids == tracker.ids && size == tracker.size && Arrays.equals(items, tracker.items);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(ids, size);
+        result = 31 * result + Arrays.hashCode(items);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int x = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item.getName() == key) {
-                rsl = item;
+            if (Objects.equals(item.getName(), key)) {
+             rsl[x++] = items[index];
+              rsl = Arrays.copyOf(items, rsl.length);
             }
         }
         return rsl;
     }
-    }
+}
+
 
 
