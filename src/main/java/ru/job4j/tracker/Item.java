64dc359737507d9f8d10ccb89a4c507a8a8ba6d8
games-pc <denis2maksimov@gmail.com>
2021-01-06 16:14:08 +0300

    9. toString [#394990]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,6 +8,14 @@ import java.util.Objects;
 
 public class Item {
     @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,27 +8,8 @@ public class StartUI {
     Tracker take = new Tracker();
     Item kata = new Item();
     take.add(kata);
-    kata.setName("Катя");
-  //  System.out.println(take.findById(1));
-    Item kata1 = new Item();
-    take.add(kata1);
-    kata1.setName("Катя");
-        Item kata3 = new Item();
-        take.add(kata3);
-        kata3.setName("Катя");
-    Item olia = new Item();
-    take.add(olia);
-    olia.setName("Оля");
-   // take.delete(1);
-   //     System.out.println(take.findById(1));
-System.out.println(Arrays.toString(take.findByName("Оля")));
- //System.out.println((take.findByName("Катя")));
- System.out.println(Arrays.toString(take.findAll()));
- //System.out.println((take.replace(1, kata1)));
-// System.out.println("deleted the index 1 " + take.delete(1));
-        System.out.println(take.findById(4).getName());
-   System.out.println(Arrays.toString(take.findByName("Катя")));
-        System.out.println(take.findByName("Оля"));
-        System.out.println(Arrays.toString(take.findAll()));
+    kata.setName("Katerina");
+        System.out.println(kata);
+
               }
 }
