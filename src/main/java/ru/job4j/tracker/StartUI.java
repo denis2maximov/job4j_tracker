80a000c5100046de42c46c832a669bde9d1b8155
games-pc <denis2maksimov@gmail.com>
2021-01-07 17:47:09 +0300

    8. Что такое валидация? [#394988]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,8 +8,18 @@ public class StartUI {
     Tracker take = new Tracker();
     Item kata = new Item();
     take.add(kata);
-    kata.setName("Katerina");
-        System.out.println(kata);
+    kata.setName("there is no replacement");
+    System.out.println(kata);
 
+    Item lol = new Item();
+  //  take.add(lol);
+  //  lol.setName("replacement made");
+  lol.setName("replacement made");
+  //  System.out.println(lol);
+    int id = kata.getId();
+    take.replace(id, kata);
+    System.out.println(take.findById(id).getName());
+        take.replace(id, lol);
+        System.out.println(take.findById(id).getName());
               }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,17 +14,6 @@ public class Tracker {
         return item;
     }
 
-  /*  public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    } */
   public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
@@ -47,21 +36,22 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-       /* int index = indexOf(id);
-       if (index == -1) {
-            return false;
-            }
-        items[index] = item;
-        item.setId(id);
-        return true; */
         int index = indexOf(id);
         boolean rsl = index != -1;
-        if (rsl) {
-            items[index] = item;
-            item.setId(id);
+        if (item.equals(items[index])) {
+            return true;
+        } else {
+            if (rsl) {
+                 {
+                    items[index] = item;
+                    item.setId(id);
+                }
+
+            }
+
         }
         return rsl;
-    }
+  }
 
     private int indexOf(int id) {
         int rsl = -1;
