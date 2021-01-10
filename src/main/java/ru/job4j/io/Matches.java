aeb75cd5c48059323f32129489c183e7b23bdc3f
games-pc <denis2maksimov@gmail.com>
2021-01-10 22:17:51 +0300

    2. Scanner и чтение числа из консоли. [#394912]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,9 +4,30 @@ import java.util.Scanner;
 
 public class Matches {
     int match;
+    int min;
+    int max;
 
-    public Matches(int x) {
-        this.match = x;
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public Matches(int match, int min, int max) {
+        this.match = match;
+        this.min = min;
+        this.max = max;
+
     }
 
     public int getMatch() {
@@ -19,53 +40,45 @@ public class Matches {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Matches model = new Matches(11);
+        System.out.println("Вы приглашаетесь сыграть в игру 'Спички'. " + " Введите общее количество спичек участвующих в игре");
+        int select = Integer.valueOf(input.nextLine());
+        System.out.println(" Введите  минимальное количество спичек, которое можно забирать  за один ход (обычно это 1)");
+        int min = Integer.valueOf(input.nextLine());
+        System.out.println(" Введите  максимальное количество спичек, которое можно забирать  за один ход");
+        int max = Integer.valueOf(input.nextLine());
+        Matches model = new Matches(select, min, max);
         boolean run = true;
         System.out.println("Вы приглашаетесь сыграть в игру 'Спички'. " +
-                "Всего 11 спичек. Можно забирать от 1 до 3 спичек за ход. " +
+                "Всего " + select + " спичек. Можно забирать от " + min + " до " + max + " спичек за ход. " +
                 "Игроки ходят по очереди. Проигрывает тот, кто взял последнюю спичку." +
                 "Вы готовы? Если готовы - выберите нужное количество спичек и нажмите Enter");
-        do  {
-            int select = Integer.valueOf(input.nextLine());
-            if (select > 0 && select <= model.getMatch()) {
-                if (model.getMatch() > 0) {
-                    if (select == 3) {
-                        int x = model.getMatch() - select;
-                        System.out.println("Осталось " + x + " спичек");
-                        model.setMatch(x);
-                        if (model.getMatch() <= 0) {
-                            System.out.println("Вы проиграли!");
-                            System.out.println("Конец игры!");
-                            break;
-                        }
+        do {
+            select = Integer.valueOf(input.nextLine());
 
-                    } else if (select == 1) {
-                        int x = model.getMatch() - select;
-                        System.out.println("Осталось " + x + " спичек");
-                        model.setMatch(x);
-                        if (model.getMatch() <= 0) {
-                            System.out.println("Вы проиграли!");
-                            System.out.println("Конец игры!");
-                            break;
+            if (select <= max && select >= min) {
+                if (select > 0 && select <= model.getMatch()) {
+                    if (model.getMatch() > 0) {
+                        if (select <= max && select >= min) {
+                            int x = model.getMatch() - select;
+                            System.out.println("Осталось " + x + " спичек");
+                            model.setMatch(x);
+                            if (model.getMatch() <= 0) {
+                                System.out.println("Вы проиграли!");
+                                System.out.println("Конец игры!");
+                                break;
+                            }
                         }
-
-                    } else if (select == 2) {
-                        int x = model.getMatch() - select;
-                        System.out.println("Осталось " + x + " спичек");
-                        model.setMatch(x);
-                        if (model.getMatch() <= 0) {
-                            System.out.println("Вы проиграли!");
-                            System.out.println("Конец игры!");
-                            break;
-                        }
-                    } else {
-                        System.out.println("Введите правильное число.");
                     }
-                }
                 } else {
-                System.out.println("Это число ввести нельзя!");
+                    System.out.println("Это число ввести нельзя!");
+                }
+
+            } else {
+                System.out.println("Введите правильное число.");
             }
-            } while (run);
-        }
+
+        } while (run) ;
     }
+}
+
 
