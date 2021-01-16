5648dc8ada77638894bcdf2b7517a4076d1310a4
games-pc <denis2maksimov@gmail.com>
2021-01-16 17:44:54 +0300

    3. Интерфейс Input [#394913]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,23 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        String nulled = "to null";
+        System.out.println(nulled);
+    }
+
+    @Override
+    public void passengers(int x) {
+       int passengers = x;
+        System.out.println("the method accepts the number of passengers equal to " + passengers
+                + " , but does not return a value");
+    }
+
+    @Override
+    public int fill(int fuel) {
+        int fuelCount = fuel * 50;
+        System.out.println("the method takes " + fuel + " and returns the value " + fuelCount);
+        return fuelCount;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,8 @@
+package ru.job4j.poly;
+
+public interface Transport {
+     void drive();
+     void passengers(int x);
+     int fill(int fuel);
+
+}
