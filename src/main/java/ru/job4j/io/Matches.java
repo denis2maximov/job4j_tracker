e2d08237bed9192bc792fd5398f597b84450596f
games-pc <denis2maksimov@gmail.com>
2021-01-16 16:37:43 +0300

    2. Scanner и чтение числа из консоли. [#394912] v7

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,84 +3,32 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
-    int match;
-    int min;
-    int max;
 
-    public int getMax() {
-        return max;
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Игра 11.");
+    boolean turn = true;
+    int count = 11;
+
+     do {
+        String player = turn ? "Первый игрок" : "Второй игрок";
+        System.out.println(player + " введите число от 1 до 3:");
+        int matches = Integer.parseInt(input.nextLine());
+                     turn = !turn;
+         if (matches < 4 && matches > 0) {
+             count = count - matches;
+             System.out.println("Осталось " + count + " спичек");
+         } else {
+             System.out.println("Введите правильное чиисло");
+         }
+
+    } while (count > 0);
+    if (!turn) {
+        System.out.println("Выиграл первый игрок");
+    } else {
+        System.out.println("Выиграл второй игрок");
     }
-
-    public void setMax(int max) {
-        this.max = max;
-    }
-
-    public int getMin() {
-        return min;
-    }
-
-    public void setMin(int min) {
-        this.min = min;
-    }
-
-    public Matches(int match, int min, int max) {
-        this.match = match;
-        this.min = min;
-        this.max = max;
-
-    }
-
-    public int getMatch() {
-        return match;
-    }
-
-    public void setMatch(int match) {
-        this.match = match;
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(" Вы приглашаетесь сыграть в игру 'Спички'. " + " Введите общее количество спичек участвующих в игре");
-        int select = Integer.valueOf(input.nextLine());
-        System.out.println(" Введите  минимальное количество спичек, которое можно забирать  за один ход (обычно это 1)");
-        int min = Integer.valueOf(input.nextLine());
-        System.out.println(" Введите  максимальное количество спичек, которое можно забирать  за один ход");
-        int max = Integer.valueOf(input.nextLine());
-        Matches model = new Matches(select, min, max);
-        boolean run = true;
-        System.out.println("Вы приглашаетесь сыграть в игру 'Спички'. " +
-                "Всего " + select + " спичек. Можно забирать от " + min + " до " + max + " спичек за ход. " +
-                "Игроки ходят по очереди. Проигрывает тот, кто взял последнюю спичку." +
-                "Вы готовы? Если готовы - выберите нужное количество спичек и нажмите Enter");
-        do {
-            select = Integer.valueOf(input.nextLine());
-
-            if (select <= max && select >= min) {  // проверка входящего числа на диапазон играющих чисел
-                if (select > 0 && select <= model.getMatch()) { // проверка на отрицательный остаток и на не  привышение остатка спичек введеному числу
-                    if (model.getMatch() > 0) { // проверка на положительный остаток спичек после предыдущих ходов
-                    //    if (select <= max && select >= min) {
-                            int x = model.getMatch() - select;
-                            System.out.println("Осталось " + x + " спичек");
-                            model.setMatch(x);
-                            if (model.getMatch() <= 0) {                 // Проверка на положительный остаток спичек после хода, проверка конца игры.
-                                System.out.println("Вы сделали последний ход в этой игре " +
-                                        "(забрали последние спички)!");
-                                System.out.println("Вы проиграли! Конец игры!");
-                                break;
-                            }
-                       // }
-                    }
-                } else {
-                    System.out.println("Это число ввести нельзя!");
-                    }
-
-                } else {
-                System.out.println("Введите правильное число.");
-            }
-
-
-        }  while (run) ;
-    }
+}
 }
 
 
