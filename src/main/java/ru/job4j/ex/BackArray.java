4338b575b46e9d5770c6dac45df18e00e4711e8a
games-pc <denis2maksimov@gmail.com>
2021-01-23 15:36:22 +0300

    0. Что такое исключение. [#394939]

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -3,15 +3,43 @@ package ru.job4j.ex;
 import java.util.Arrays;
 
 public class BackArray {
+
     public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        int middle = names.length / 2;
+        for (int index = 0; index < middle; index++) {
+            String temp = names[index];
+            names[index] = names[(names.length -1) - index];
+            names[(names.length - 1) - index] = temp;
+        }
+        System.out.println(Arrays.toString(names));
+    }
+
+ /* public static void main(String[] args) {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+     System.out.println(Arrays.toString(names));
+        int middle = names.length / 2;
+        for (int index = 0; index < middle; index++) {
+            String temp = names[index];
+            names[index] = names[middle - index];
+            names[middle - index] = temp;
+          // System.out.println(Arrays.toString(names));
+        }
+        System.out.println(Arrays.toString(names));
+    } */
+
+
+  /* public static void main(String[] args) {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+       System.out.println(Arrays.toString(names));
         int middle = names.length / 2;
         for (int index = 0; index <= middle; index++) {
             String temp = names[index];
             names[index] = names[names.length - 1];
             names[names.length - 1] = temp;
+            System.out.println(Arrays.toString(names));
         }
         System.out.println(Arrays.toString(names));
-    }
+    } */
 }
 
