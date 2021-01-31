e00d30f1290a75bd931a5dd603222e05199b7bf5
denis2maksimov <denis2maksimov@gmail.com>
2021-01-31 21:13:27 +0300

    1.1. Тесты на StartUI. [#394936]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,8 +5,32 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.nullValue;
 
-public class StartUITest {
+ public class StartUITest {
     @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"8", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                                + "=== Exit Program ====%n"
+                )
+        ));
+    }
+}
+
+  /*  @Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
