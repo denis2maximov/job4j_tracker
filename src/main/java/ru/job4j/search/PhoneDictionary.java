545140d59b79241aed17ef669c828fdf175608f6
Denis <denis2maksimov@gmail.com>
2021-03-24 20:03:02 +0300

    1. Телефонный справочник на базе ArrayList [#394890] - вторая попытка

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -11,15 +12,38 @@ public class PhoneDictionary {
 
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for ( Person rsl : persons) {
-            rsl.contains(key);
-            result.add(rsl);
+        for (Person rsl : persons) {
+         /*  if (rsl.getPhone().contains(key)) {
+                result.add(rsl);
+            } else if (rsl.getSurname().contains(key)) {
+                result.add(rsl);
+            } else if (rsl.getAddress().contains(key)) {
+                result.add(rsl);
+             } else if (rsl.getName().contains(key)) {
+                result.add(rsl);
+            } else  {
+                return rsl.isEmpty();
+            } */
+           if (rsl.getPhone().contains(key)) {
+                result.add(rsl);
+            }
+            if (rsl.getSurname().contains(key)) {
+                result.add(rsl);
+            }
+            if (rsl.getAddress().contains(key)) {
+                result.add(rsl);
+            }
+            if (rsl.getName().contains(key)) {
+                result.add(rsl);
+            }
         }
         return result;
     }
 }
+
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,26 +3,40 @@ package ru.job4j.search;
 import org.junit.Test;
 
 import java.util.ArrayList;
-
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
 
- /*   @Test
+  /*  @Test
     public void add() {
-    } */
-        @Test
-        public void whenFindByName() {
-            PhoneDictionary phones = new PhoneDictionary();
-            phones.add(
-                    new Person("Petr", "Arsentev", "534872", "Bryansk")
-            );
-            phones.add(
-                    new Person("Denis", "Mahnov", "125222", "Moscow")
-            );
-            ArrayList<Person> persons = phones.find("Den");
-           assertThat(persons.get(1).getSurname(), is("Mahnov"));
-           // assertThat(persons.contains("Petr"), is("Arsentev"));
-        }
+    }*/
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Denis", "SuperStar", "127422", "Murmansk")
+        );
+        phones.add(
+                new Person("Denis", "HromMolibden", "127422", "Voroneg")
+        );
+        ArrayList<Person> prs = phones.find("422");
+       // assertThat(prs.get(0).getSurname(), is("SuperStar"));
+        prs.forEach((num)-> System.out.println(num));
+        assertThat(prs.get(1).getSurname(), is("HromMolibden"));
     }
+    @Test
+    public void whenFindByNameNull() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Denis", "SuperStar", "127422", "Murmansk")
+        );
+        ArrayList<Person> prs = phones.find("Ololoshka");
+        assertThat(prs.isEmpty(), is(true));
+    }
+}
