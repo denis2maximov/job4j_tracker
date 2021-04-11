8e420e1544e66a197c5eb9fdc770104d2235bb6b
dss-vaio <denis2maksimov@gmail.com>
2021-04-11 16:32:28 +0300

    0. Set, HashSet, Iterator. [#394906]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+
+        public static void main(String[] args) {
+            HashSet<String> autos = new HashSet<String>();
+            autos.add("Lada");
+            autos.add("BMW");
+            autos.add("Volvo");
+            autos.add("Audi");
+            autos.add("Toyota");
+            for (String string : autos  ) {
+                System.out.println(string);
+            }
+        }
+    }
+
