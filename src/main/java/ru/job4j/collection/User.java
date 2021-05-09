f1deede4eebe6a304da143d83c216ed40561802e
DSS-VAIO <denis2maksimov@gmail.com>
2021-05-09 17:14:48 +0300

    1. Организовать сортировку User [#394886]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,46 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int i = name.compareTo(o.getName());
+        if (i != 0) return i;
+        i = age - (o.getAge());
+        if (i != 0) return i;
+        return Integer.compare(age, o.age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+}
