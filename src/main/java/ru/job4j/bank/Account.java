8a42cea7b514612efec9d702e96c9a29506457e7
DSS-VAIO <denis2maksimov@gmail.com>
2021-05-22 12:22:44 +0300

    3. Что такое JavaDoc [#394877]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,64 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Модель аккаунта пользователя. Две переменных:
+ * реквизиты счета и баланс.
+ */
 public class Account {
     private String requisite;
     private double balance;
 
+    /**
+     * конструктор модели аккаунт.
+     * @param requisite - строковое представление счета
+     * @param balance - числовое представление
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * получение реквизитов аккаунта
+     * @return requisite
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * запись реквизитов аккаунта.
+     * принимает строковое значение
+     * @param requisite
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
+
+    /**
+     * получение баланса аккаунта
+     * @return
+     */
 
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * запись баланса аккаунта
+     * @param balance
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * переопределяем метод equals для возможности
+     * сравнения аккаунтов. Сравниваем по реквизитам.
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +72,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * переопределяем метод, что бы хэшкод аккаунта был
+     * requisite
+     * @return requisite
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,59 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Модель пользователя. Имеет 2 переменные passport  и username.
+  */
 public class User {
     private String passport;
     private String username;
 
+    /**
+     *Конструтор шаблона User, принимает на вход строковые параметры:
+     * @param passport - паспорт пользователя
+     * @param username - имя пользователя.
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Геттер паспорта юзера, обспечивает получение паспорта по User
+    */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Сеттер паспорта юзера, обеспечивает запись паспорта юзеру
+     * @param passport - паспорт пользователя
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод обеспечивает получение (возвращаемое значчение)  Username
+     * @return  - username Имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод обеспечивает запись имени пользовтеля.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * переопрееление equals (равнения) что бы можно было сравнивать
+     * объект User. Для сравнения выбрана переменная passport
+     * @param o - лобъект типа User
+     * @return false или true (верно \ ложно)
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +67,11 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     *переопределяем метод , что бы хэшкод объекта
+     * User было passport
+     * @return passport
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
