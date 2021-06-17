d3c72ec9ce4de312622e3b1e83eae2eb8ef6f222
DSS-VAIO <denis2maksimov@gmail.com>
2021-06-17 20:47:27 +0300

    2. Отсортировать департаменты [#34352] правка 3.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -4,10 +4,9 @@ import java.util.*;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 12;
         String[] left = o1.split("/");
         String[] right = o2.split("/");
-        rsl = right[0].compareTo(left[0]);
+        int rsl = right[0].compareTo(left[0]);
         return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
