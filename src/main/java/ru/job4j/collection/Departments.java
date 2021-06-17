265aab7d2f2dc66472d98bdbc3c6f01a50a71e65
DSS-VAIO <denis2maksimov@gmail.com>
2021-06-17 08:18:52 +0300

    2. Отсортировать департаменты [#34352] пересылка файлов

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -7,8 +7,59 @@ public class Departments {
     public static List<String> fillGaps(List<String> deps) {
         Set<String> tmp = new LinkedHashSet<String>();
         List<String> rsl = new ArrayList<String>();
-        String start = "null";
-        for (String out : deps) {
+           for (String out : deps) {
+            String start = "";
+            for (String value : out.split("/")) {
+                if ("".equals(start)) {
+                     start = value;
+                } else {
+                    start += "/" + value;
+                }
+                 tmp.add(start);
+                 }
+            }
+        System.out.println(tmp);
+           rsl.addAll(tmp);
+            return new ArrayList<>(rsl);
+        }
+
+    public static void sortAsc(List<String> orgs) {
+        List<String> values = new ArrayList<>(orgs);
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static List<String> sortDesc(List<String> orgs) {
+        List<String> values = new ArrayList<>(orgs);
+        Comparator<String> comp = new DepDescComp();
+      //  values.sort(DepDescComp);
+        return values;
+    }
+
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("k1/sk2/ssk1", "k2/sk1/ssk2");
+        List<String> result = Departments.fillGaps(input);
+        List<String> out = sortDesc(result);
+     //  sortAsc(result);
+       // result.forEach(System.out::println);
+        //sortDesc(result);
+     out.forEach(System.out::println);
+
+        for (int i = 0; i < result.size(); i++) {
+            String one = null;
+           // System.out.println(one[i]);
+        } {
+           // System.out.println(string);
+        }
+
+    //    System.out.println(result);
+    }
+
+
+/*
+Set<String> tmp = new LinkedHashSet<String>();
+        List<String> rsl = new ArrayList<String>();
+           for (String out : deps) {
+            String start = "";
             for (String value : out.split("/")) {
              if (tmp.size() < 1 && !start.equals(value)) {
                     tmp.add(value);
@@ -58,4 +109,6 @@ public class Departments {
         sortDesc(result);
         result.forEach(System.out::println);
     }
+}
+ */
 }
