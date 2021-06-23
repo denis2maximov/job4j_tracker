d38ac20a29df0f27ae97543c206f5af22f939134
dss <denis2maksimov@gmail.com>
2021-06-23 18:48:06 +0300

    2.2. Лямбда блок [#249208] перенос файлов

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+/*
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String left = "Hello";
+        String  right = "World";
+        System.out.println("compare - " + left.length() + " : " + right.length());
+        Comparator<String> cmpDescSize = (left, right)
+                -> Integer.compare(right.length(), left.length());
+    }
+
+}
+*/
