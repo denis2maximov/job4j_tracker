862e5dc576988a67b9241bf7380d781be133e919
dss <denis2maksimov@gmail.com>
2021-06-28 17:24:39 +0300

    5. Подсчет функции в диапазоне. [#24251] сократил согласно замечанию

--- src/main/java/ru/job4j/function/CountFunc.java
+++ src/main/java/ru/job4j/function/CountFunc.java
@@ -2,7 +2,6 @@ package ru.job4j.function;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -10,8 +9,7 @@ public class CountFunc {
     public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> doubles = new ArrayList<Double>();
         for (int i = start; i < end; i++) {
-            Function<Integer, Double> funcX = x -> (double) x;
-            doubles.add(func.apply(funcX.apply(i)));
+            doubles.add(func.apply((double) i));
         }
         return  doubles;
     }
