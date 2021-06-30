1190fb1d2cb2bfb40d62dad2be0e38f1ecb5b33d
dss <denis2maksimov@gmail.com>
2021-06-30 17:50:43 +0300

    0. Stream API [#58166]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,14 +1,15 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsage {
     public static void main(String[] args) {
         ArrayList<Integer> now  = new ArrayList<Integer>();
-        for (Integer i = -50; i < 5; i++) {
+        for (int i = -50; i < 5; i++) {
             now.add(i);
         }
-       now.stream().filter(s -> s > 0).collect(Collectors.toList());
-    }
+        List<Integer> listik = now.stream().filter(s -> s > 0).collect(Collectors.toList());
+       }
 }
