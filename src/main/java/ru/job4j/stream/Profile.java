deb66709d9ccd09d9c6c32215895520089b52b03
dss <denis2maksimov@gmail.com>
2021-07-05 15:43:30 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private String name;
+    private Address address;
+
+    public Profile(String name, Address address) {
+        this.address = address;
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenCollectClassV() {
+        List<Profile> profile = List.of(
+                new Profile("Oleg", new Address("Saratov", "Lenina", 1, 1)),
+                new Profile("Aleg", new Address("Moscow", "Lenina", 101, 1)),
+                new Profile("Motia", new Address("Moscow", "Leningradskiy pr.", 24, 17))
+        );
+        List<Address> rsl = Profiles.collect(profile);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Saratov", "Lenina", 1, 1));
+        expected.add(new Address("Moscow", "Lenina", 101, 1));
+        expected.add(new Address("Moscow", "Leningradskiy pr.", 24, 17));
+        assertThat(rsl, is(expected));
+    }
+}
